d8e5419cbb6a3129dc3d21aee62aefa447843927
Alix Fachin <alix.fachin@me.com>
2021-04-16 08:24:30 +0900

    Setting up basic scaffolding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import "./keyGenerator";
+import  KeyGenerator from './keyGenerator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1> Easy Crypto!</h1>
+        <p> Trying to help you understand crypto </p>
+
+        <KeyGenerator />
+
+      </div>
+    </>
   );
 }
 
