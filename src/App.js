07657dcb56711651260ab90f730a68118af19724
Alix Fachin <alix.fachin@me.com>
2021-04-18 14:22:22 +0900

    Less crowded landing page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import './App.css';
 import  KeyGenerator from './keyGenerator';
 import EncodingPanel from './encodingPanel';
 import DecodingPanel from './decodingPanel';
+import React, {useState} from 'react';
 
 
 function App() {
+  const [currentPanel,setCurrentPanel] = useState('keygen');
+  
+  function onKeyGenClick() {
+    setCurrentPanel('keygen');
+  }
+  function onEncodingClick() {
+    setCurrentPanel('encode');
+  }
+
   return (
     <>
       <div className="App">
-        <h1> Easy Crypto!</h1>
-        <p> Trying to help you understand crypto </p>
-        <KeyGenerator />
-        <EncodingPanel />
-        <DecodingPanel />
+        <header> <h1> Easy Crypto!</h1>
+        <p> Easy implementation of the RSA algorithm </p>
+        <div class="button-row">
+          <button onClick={ onKeyGenClick}>Key Generator</button>
+          <button onClick={ onEncodingClick}>Encoding/Decoding</button>
+        </div>
+        </header>
+        { currentPanel === 'keygen' ? <KeyGenerator /> :  <> <EncodingPanel /> <DecodingPanel /> </> }
+        
       </div>
     </>
   );
